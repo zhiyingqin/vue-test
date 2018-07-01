cfd888b7964b852fa4eaa6281f2a5b58f0f7f1ed
v_zhiyqin <v_zhiyqin@tencent.com>
2018-07-01 17:42:54 +0800

    暂存

---
+++ src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <header></header>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      msg: 'Welcome to my project'
+    }
+  }
+}
+</script>
+<style scoped>
+header {
+	width: 100%;
+	height: 60px;
+	border: 1px solid lightgray;
+	position: fixed;
+	top: 0;
+	z-index: 1;
+}
+</style>
